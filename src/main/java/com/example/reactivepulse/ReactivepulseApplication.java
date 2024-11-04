b2d234079402e995c1e1d5eac7f7896f9ad5363d
Sagar Phalke <sagar.phalke@giift.com>
2024-11-04 12:07:49 +0530

    Initial commit

---
+++ src/main/java/com/example/reactivepulse/ReactivepulseApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactivepulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivepulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivepulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactivepulse/ReactivepulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactivepulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivepulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
